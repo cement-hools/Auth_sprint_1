c59472f6d00777d7c1b3c9405d5ff2748d4f3e74
Andrey Lepekhin <andrey@lepekhin.com>
2023-02-10 12:49:22 +0600

    Add pre-commit hook config, also run it on main.

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
-
 from settings import flask_settings
-from .db import init_db
+
 from .api import v1
+from .db import init_db
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = flask_settings.secret_key
+    app.config["SECRET_KEY"] = flask_settings.secret_key
 
     app.register_blueprint(v1.bp)
 
--- src/app/api/v1/auth.py
+++ src/app/api/v1/auth.py
@@ -1,10 +1,10 @@
 from http import HTTPStatus
 
-from flask import jsonify, Blueprint
+from flask import Blueprint, jsonify
 
-router = Blueprint('auth', __name__)
+router = Blueprint("auth", __name__)
 
 
-@router.route('/login', methods=['GET'])
+@router.route("/login", methods=["GET"])
 def login():
-    return jsonify(message='hello world!'), HTTPStatus.OK
+    return jsonify(message="hello world!"), HTTPStatus.OK
