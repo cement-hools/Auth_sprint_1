713f839f9e504e9543199ec88c4afc8b2be23d3c
Maxim Sekachev <cement-fan@ya.ru>
2023-02-11 13:36:36 +0300

    Добавил endpoints для CRUD ролей

--- src/app/api/v1/__init__.py
+++ src/app/api/v1/__init__.py
@@ -1,6 +1,8 @@
-from app.api.v1 import auth
 from flask import Blueprint
+
+from app.api.v1 import auth, roles
 
 bp = Blueprint("v1", __name__, url_prefix="/api/v1")
 
 bp.register_blueprint(auth.router)
+bp.register_blueprint(roles.router)
