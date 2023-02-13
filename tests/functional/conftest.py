017edffb12fd59fcfc4b0e5be0b22e82c714844a
Andrey Lepekhin <andrey@lepekhin.com>
2023-02-13 21:40:50 +0600

    scaffolding for tests

---
+++ tests/functional/conftest.py
@@ -0,0 +1,14 @@
+"""Fixtures"""
+import asyncio
+
+import pytest
+
+from tests.functional.fixtures.async_http import (
+    aiohttp_client_session,
+    aiohttp_get,
+    aiohttp_post,
+)
+
+
+def pytest_configure():
+    pytest.strange_unicode_str = "\uFFFF~𝘈Ḇ𝖢𝕯٤ḞԍНǏ𝙅ƘԸⲘ𝙉০Ρ𝗤Ɍ𝓢ȚЦ𝒱Ѡ𝓧ƳȤѧᖯć𝗱ễ𝑓𝙜Ⴙ𝞲𝑗𝒌ļṃŉо𝞎𝒒ᵲꜱ𝙩ừ𝗏ŵ𝒙𝒚ź���!@#$%^&*()大-_=+[{]};:"
