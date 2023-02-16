5700e368b56399654ca80aba8dcbdca89a5f9251
Andrey Lepekhin <andrey@lepekhin.com>
2023-02-16 19:56:30 +0600

    Move JWT app details to a separate file.

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,12 +1,9 @@
 from datetime import timedelta
 
 from flask import Flask
-from flask_jwt_extended import JWTManager
-from flask_redis import FlaskRedis
 
+from app.jwt_app import jwt, jwt_redis_blocklist
 from settings import flask_settings, jwt_settings, redis_settings
-
-jwt_redis_blocklist = FlaskRedis()
 
 
 def create_app():
@@ -22,32 +19,7 @@ def create_app():
     app.config["REDIS_URL"] = redis_settings.dsn
 
     jwt_redis_blocklist.init_app(app, decode_responses=True)
-
-    jwt = JWTManager(app)
-
-    @jwt.user_identity_loader
-    def user_identity_lookup(user):
-        """
-        Serializes user for storing in a JWT token.
-        """
-        return user.id
-
-    @jwt.user_lookup_loader
-    def user_lookup_callback(_jwt_header, jwt_data):
-        """
-        Coverts user from a JWT token into User object.
-        """
-        identity = jwt_data["sub"]
-        return User.query.filter_by(id=identity).one_or_none()
-
-    @jwt.token_in_blocklist_loader
-    def check_if_token_is_revoked(jwt_header, jwt_payload: dict):
-        """
-        Callback function to check if a JWT exists in the redis blocklist.
-        """
-        jti = jwt_payload["jti"]
-        token_in_redis = jwt_redis_blocklist.get(jti)
-        return token_in_redis is not None
+    jwt.init_app(app)
 
     app.register_blueprint(v1.bp)
 
---
+++ src/app/jwt_app.py
@@ -0,0 +1,35 @@
+from flask_jwt_extended import JWTManager
+from flask_redis import FlaskRedis
+
+from app.db.models.user import User
+
+jwt_redis_blocklist = FlaskRedis()
+
+jwt = JWTManager()
+
+
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    """
+    Serializes user for storing in a JWT token.
+    """
+    return user.id
+
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    """
+    Coverts user from a JWT token into User object.
+    """
+    identity = jwt_data["sub"]
+    return User.query.filter_by(id=identity).one_or_none()
+
+
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload: dict):
+    """
+    Callback function to check if a JWT exists in the redis blocklist.
+    """
+    jti = jwt_payload["jti"]
+    token_in_redis = jwt_redis_blocklist.get(jti)
+    return token_in_redis is not None
