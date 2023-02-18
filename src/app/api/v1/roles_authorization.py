1d20c4193c4b3ac3de88d8cd3ba65bc0c41e5b39
Andrey Lepekhin <andrey@lepekhin.com>
2023-02-18 21:44:55 +0600

    Swap explicit role uuids for names

--- src/app/api/v1/roles_authorization.py
+++ src/app/api/v1/roles_authorization.py
@@ -3,17 +3,13 @@ from functools import wraps
 from flask import abort
 from flask_jwt_extended import current_user, verify_jwt_in_request
 
-from settings import user_roles_settings
-
 
 def _has_roles_or_abort(roles):
     verify_jwt_in_request()
 
-    roles_uuids = [user_roles_settings.uuids[role] for role in roles]
-
     user_roles = current_user.roles
 
-    if not any(str(role.id) in roles_uuids for role in user_roles):
+    if not any(role.name in roles for role in user_roles):
         abort(403, "Access denied")
 
 
