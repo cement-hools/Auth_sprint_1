4515522a0fb222ab95c11c8adf3e5c29b9bb1ed8
Andrey Lepekhin <andrey@lepekhin.com>
2023-02-19 18:25:39 +0600

    Move logic out of routes to services.

--- src/app/api/v1/auth/handlers.py
+++ src/app/api/v1/auth/handlers.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from http import HTTPStatus
 
 from flask import Blueprint, request
@@ -8,17 +7,14 @@ from app.api.v1.auth.schemas import (
     ChangePasswordUserRequest,
     LoginHistoryData,
     LoginUserRequest,
-    LoginUserResData,
     LogoutUser,
     RegUserRequest,
     RoleData,
 )
-from app.db import db, models
-from settings import logger
+from app.services import auth
 
 from ..schemas import BaseResponse
 from ..utils import get_body
-from .utils import create_access_and_refresh_jwt, invalidate_jwt
 
 router = Blueprint("auth", __name__)
 
@@ -30,49 +26,17 @@ def registration():
     No auth header needed.
     """
     body: RegUserRequest = get_body(RegUserRequest)
-    login_in_db = models.User.query.filter_by(login=body.login).one_or_none()
-    if login_in_db:
-        error_message = "Login already exists"
-        return (
-            BaseResponse(success=False, error=error_message).dict()
-        ), HTTPStatus.BAD_REQUEST
-
-    user = models.User(**body.dict())
-    db.session.add(user)
-    db.session.commit()
-
-    return BaseResponse(success=True, error="").dict(), HTTPStatus.OK
-
-
-@router.route("/password_change", methods=["POST"])
-@jwt_required()
-def password_change():
-    """
-    User password change.
-    """
-    body: ChangePasswordUserRequest = get_body(ChangePasswordUserRequest)
-
-    if current_user.verify_password(body.old_password):
-        current_user.password = body.new_password
-
-        # Old tokens invalidation
-        for token in models.JWTStore.query.filter_by(
-            user_id=current_user.id
-        ).all():
-            invalidate_jwt(token.jwt_id, token.type)
-            db.session.delete(token)
-        db.session.commit()
-    else:
-        error_message = "Wrong password"
-        return (
-            BaseResponse(success=False, error=error_message).dict()
-        ), HTTPStatus.UNAUTHORIZED
-
-    jwt_tokens = create_access_and_refresh_jwt(current_user)
-    return (
-        BaseResponse(data=jwt_tokens).json(),
-        HTTPStatus.OK,
+    result = auth.registration(
+        login=body.login,
+        email=body.email,
+        password=body.password,
     )
+    if result.error_message:
+        return (
+            BaseResponse(success=False, error=result.error_message).dict()
+        ), HTTPStatus.BAD_REQUEST
+    else:
+        return BaseResponse(success=True, error="").dict(), HTTPStatus.OK
 
 
 @router.route("/login", methods=["POST"])
@@ -82,111 +46,110 @@ def login():
     """
     body: LoginUserRequest = get_body(LoginUserRequest)
 
-    user = models.User.query.filter_by(login=body.login).one_or_none()
-    if not user or not user.verify_password(body.password):
+    result = auth.login(
+        login=body.login,
+        password=body.password,
+    )
+
+    if result.error_message:
         return (
             BaseResponse(
                 success=False, error="Wrong username or password"
             ).json(),
             HTTPStatus.UNAUTHORIZED,
         )
-
-    db.session.add(
-        models.LoginHistory(
-            user_id=user.id,
-            ip=request.environ.get("HTTP_X_REAL_IP", request.remote_addr),
-            user_agent=request.headers.get("User-Agent"),
-            datetime=datetime.now(),
+    else:
+        return (
+            BaseResponse(data=result.data).dict(),
+            HTTPStatus.OK,
         )
-    )
-    db.session.commit()
-    jwt_tokens = create_access_and_refresh_jwt(user)
-    return (
-        BaseResponse(data=jwt_tokens).json(),
-        HTTPStatus.OK,
-    )
 
 
-@router.route("/logout", methods=["DELETE"])
+@router.route("/password_change", methods=["POST"])
+@jwt_required()
+def password_change():
+    """
+    User password change.
+    """
+    body: ChangePasswordUserRequest = get_body(ChangePasswordUserRequest)
+    result = auth.password_change(
+        user=current_user,
+        old_password=body.old_password,
+        new_password=body.new_password,
+    )
+    if result.error_message:
+        return (
+            BaseResponse(success=False, error=result.error_message).dict()
+        ), HTTPStatus.UNAUTHORIZED
+    else:
+        return (
+            BaseResponse(data=result.data).dict(),
+            HTTPStatus.OK,
+        )
+
+
+@router.route("/logout", methods=["POST"])
 @jwt_required()
 def logout():
     """
     User logout.
-    To logout client must make two requests, one for each JWT type.
     """
     body: LogoutUser = get_body(LogoutUser)
 
     current_tokens = [get_jwt()["jti"], get_jti(body.refresh_token)]
 
-    for token in models.JWTStore.query.filter_by(
-        user_id=current_user.id
-    ).all():
-        if token.jwt_id in current_tokens:
-            invalidate_jwt(token.jwt_id, token.type)
-            db.session.delete(token)
-    db.session.commit()
+    result = auth.logout(current_user, current_tokens)
 
-    return BaseResponse(success=True, error="").json(), HTTPStatus.OK
+    return BaseResponse(success=result.success).dict(), HTTPStatus.OK
 
 
 @router.route("/logout_all", methods=["POST"])
 @jwt_required()
-def logout_all():
+def logout_all_other():
     """
     User logout all other sessions.
     """
     body: LogoutUser = get_body(LogoutUser)
 
     current_tokens = [get_jwt()["jti"], get_jti(body.refresh_token)]
-    for token in models.JWTStore.query.filter_by(
-        user_id=current_user.id
-    ).all():
-        if token.jwt_id not in current_tokens:
-            invalidate_jwt(token.jwt_id, token.type)
-            db.session.delete(token)
-    db.session.commit()
-    return BaseResponse(success=True, error="").json(), HTTPStatus.OK
+    result = auth.logout_other_tokens(current_user, current_tokens)
+    return BaseResponse(success=result.success).dict(), HTTPStatus.OK
 
 
 @router.route("/refresh", methods=["POST"])
 @jwt_required(refresh=True)  # Only allow refresh tokens to access this route.
-def refresh():
+def refresh_token():
     """
     Takes refresh JWT token as input, issues a new access and refresh token pair.
     """
     old_refresh_token_jti = get_jwt()["jti"]
-    old_refresh_token = models.JWTStore.query.filter_by(
-        user_id=current_user.id, jwt_id=old_refresh_token_jti
-    ).one_or_none()
-    if old_refresh_token:
-        db.session.delete(old_refresh_token)
-        db.session.commit()
-    invalidate_jwt(old_refresh_token_jti, "refresh")
-
-    jwt_tokens = create_access_and_refresh_jwt(current_user)
+    result = auth.refresh_token(current_user, old_refresh_token_jti)
 
     return (
-        BaseResponse(data=jwt_tokens).json(),
+        BaseResponse(success=result.success, data=result.data).dict(),
         HTTPStatus.OK,
     )
 
 
 @router.route("/user/login_history", methods=["GET"])
 @jwt_required()
-def user_login_history():
+def login_history():
     """
     User login history.
     """
-    data: list[LoginHistoryData] = []
-    for row in models.LoginHistory.query.filter_by(
-        user_id=current_user.id
-    ).all():
-        data.append(
-            LoginHistoryData(
-                ip=row.ip, user_agent=row.user_agent, datetime=row.datetime
-            )
-        )
-    return BaseResponse(data=data).json(), HTTPStatus.OK
+    page = request.args.get("page", default=1, type=int)
+    per_page = request.args.get("per_page", default=10, type=int)
+    result = auth.login_history_w_pagination(
+        user=current_user,
+        page=page,
+        per_page=per_page,
+    )
+    return (
+        BaseResponse(
+            success=result.success, data=LoginHistoryData(**result.data)
+        ).dict(),
+        HTTPStatus.OK,
+    )
 
 
 @router.route("/user/roles", methods=["GET"])
@@ -195,8 +158,11 @@ def user_roles_list():
     """
     User roles list.
     """
+    result = auth.roles_list(current_user)
     data: list[RoleData] = []
-    for role in current_user.roles:
-        data.append(RoleData(id=role.id, name=role.name))
-
-    return BaseResponse(data=data).json(), HTTPStatus.OK
+    for role in result.data:
+        data.append(RoleData(**role))
+    return (
+        BaseResponse(success=result.success, data=data).dict(),
+        HTTPStatus.OK,
+    )
