f9c6389e9b60bebd9fe9c029576ff55216a728e8
sekachev <sekachev@proactor.pro>
2023-03-02 13:35:53 +0300

    ondelete="CASCADE"

--- src/app/db/models/jwt.py
+++ src/app/db/models/jwt.py
@@ -1,7 +1,5 @@
 import uuid
 
-from sqlalchemy import UUID, ForeignKey
-
 from app.db import db
 
 from .user import User
@@ -11,7 +9,7 @@ class JWTStore(db.Model):
     __tablename__ = "jwt_store"
 
     id = db.Column(
-        UUID(as_uuid=True),
+        db.UUID(as_uuid=True),
         primary_key=True,
         default=uuid.uuid4,
         unique=True,
@@ -21,5 +19,5 @@ class JWTStore(db.Model):
 
     jwt_id = db.Column(db.String(36), nullable=False, index=True)
     expiration_date = db.Column(db.DateTime, nullable=False)
-    user_id = db.Column(UUID, ForeignKey(User.id))
+    user_id = db.Column(db.UUID, db.ForeignKey(User.id, ondelete="CASCADE"))
     type = db.Column(db.String(32), nullable=False)
